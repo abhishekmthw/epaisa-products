84be76631640e4f8b66cea176ac86a32f41c675a
abishekmthw <95abhishekmathew95@gmail.com>
2021-07-23 14:13:55 +0530

    Added mongo uri

--- server.js
+++ server.js
@@ -5,7 +5,8 @@ const mongoose = require('mongoose')
 
 const app = express()
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+const connectionOptions = { useCreateIndex: true, useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false };
+mongoose.connect(process.env.MONGODB_URI || config.connectionString, connectionOptions);
 const db = mongoose.connection
 db.on('error', (error) => console.error(error))
 db.on('open', () => console.log('Connected to Database'))
