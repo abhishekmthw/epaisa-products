7056054886375aa10dac989c9e9fe08bc04c6e5e
abishekmthw <95abhishekmathew95@gmail.com>
2021-07-23 14:04:19 +0530

    Added port

--- server.js
+++ server.js
@@ -15,4 +15,5 @@ app.use(express.json())
 const router = require('./routes/products')
 app.use('/products', router)
 
-app.listen(3000, () => console.log("Server Started"))
+const port = process.env.NODE_ENV === 'production' ? (process.env.PORT || 80) : 4000;
+app.listen(port, () => console.log("Server listening on port " + port))
